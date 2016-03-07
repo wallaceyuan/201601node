4cb90bf75f257282c1b15c3866773fbee3d2050f
wallaceyuan <1272864289@qq.com>
2016-03-07 17:32:06 +0800

    reg

--- 7.http/7.query.js
+++ 7.http/7.query.js
@@ -1,4 +1,7 @@
 /**
+ * Created by yuan on 2016/3/7.
+ */
+/**
  * Created by Yuan on 2016/3/6.
  */
 var http = require('http');
@@ -6,8 +9,7 @@ var mime = require('mime');
 var url = require('url');//对URL进行处理，把字符串转成对象
 var fs = require('fs');
 
-var users = [ { username: '袁圆', age: '12' }, { username: '张三', age: '12' }, { username: '李四', age: '22' } ];
-
+var users = [{ username: '钟汉良', age: '41' },{ username: '袁圆', age: '12' }, { username: '张三', age: '12' }, { username: '李四', age: '22' } ];
 function server(request,response){
     var urlObject = url.parse(request.url,true);
     var pathname = urlObject.pathname;
@@ -22,12 +24,30 @@ function server(request,response){
         request.on('data',function(data){
             str += data.toString();
             users.push(JSON.parse(str));
-            console.log(users);
         });
         request.on('end',function(){
             response.end(JSON.stringify(str));
         });
-
+    }else if(pathname =='/query'){
+        var str = ''
+        request.on('data',function(data){
+            str += data.toString();
+        });
+        request.on('end',function(){
+            var userObj = '';
+            users.forEach(function (user) {
+                if(user.username == str){
+                    userObj = user;
+                    return
+                }
+            });
+            if(userObj){
+                response.end(JSON.stringify(userObj));
+            }else{
+                response.statusCode = 302;
+                response.end();
+            }
+        });
     }else if(pathname =='/clock'){
         response.end(new Date().toLocaleString());
     }else if(pathname =='/apple'){
