715f70e8820a45a4229663a22dd091f98a9d22d5
wallaceyuan <1272864289@qq.com>
2016-03-15 22:55:11 +0800

    restful

--- 13.express/1.express.js
+++ 13.express/1.express.js
@@ -5,7 +5,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function(req,res){
+app.get('/user/:id',function(req,res){
     res.send('Hello World');
 });
 
--- 13.express/2.中间件.js
+++ 13.express/2.中间件.js
@@ -1,24 +1,54 @@
-/**
- * Created by Yuan on 2016/3/13.
- */
+//加载express
 var express = require('express');
-
+//获取配置对象
 var app = express();
+//计算一个处理请求一共花了多少时间
+/**
+ * 开始时间
+ */
+app.use(function(req,res,next){
+    res.start  = Date.now();
+    //console.time('cost');
+    //暂存原来的end方法
+    var originalEnd = res.end;
+    //为res.end重新赋值为我们自定义函数
+    res.end = function(){
+        //先把原来的end方法调用一次
+        originalEnd.apply(res,Array.prototype.slice.call(arguments));
+        //加入自己的小逻辑
+        //console.timeEnd('cost')
+        console.log('timecost',Date.now() - res.start);
+    }
+    next();
+});
+//中央
+app.use('/money',function(req,res,next){
+    res.mny = 100;
+    next();
+});
 
-app.use(function(req,res,next){
-    res.mmy = 100;
-    next();
-});
-app.use(function(req,res,next){
-    res.mmy = res.mmy -10;
-    next();
-});
-app.use(function(req,res,next){
-    res.mmy = res.mmy -20;
+//省里
+app.use('/hello',function(req,res,next){
+    res.mny = res.mny - 10;
+
     next();
 });
 
+//市里
+app.use('/money',function(req,res,next){
+    res.mny = res.mny - 30;
+
+    next();
+});
+//村
+app.use(function(req,res,next){
+    res.mny = res.mny - 60;
+    next();
+});
+
+//发送补贴100
 app.get('/money',function(req,res){
-    console.log(''+res.mny);
+    res.send(""+res.mny);
 });
-app.listen(3002);
+
+app.listen(8080);
